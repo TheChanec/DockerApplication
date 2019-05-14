70c3b227567206573d8f258e47658c3c43936fe8
Enrique Chan <enrique.nec@gmail.com>
2019-05-14 16:13:37 -0500

    Create DockerProjectContext.cs

---
+++ DockerProject/Data/DockerProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DockerProject;
+
+namespace DockerProject.Models
+{
+    public class DockerProjectContext : DbContext
+    {
+        public DockerProjectContext (DbContextOptions<DockerProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DockerProject.EntityTest> EntityTest { get; set; }
+    }
+}
