0f67c09220e6365e7e4c2bcdb15d3a46b2ea0464
Enrique Chan <enrique.nec@gmail.com>
2019-05-14 16:14:28 -0500

    Update Startup.cs

--- DockerProject/Startup.cs
+++ DockerProject/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using DockerProject.Models;
 
 namespace DockerApplication
 {
@@ -26,6 +28,46 @@ namespace DockerApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //services.AddDbContext<DockerProjectContext>(options =>
+            //        options.UseNpgsql(Configuration.GetConnectionString("DockerProjectContext")));
+
+            services.AddEntityFrameworkNpgsql().AddDbContext<DockerProjectContext>(options =>
+            {
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                string connStr;
+
+                // Depending on if in development or production, use either Heroku-provided
+                // connection string, or development connection string from env var.
+                if (env == "Development")
+                {
+                    connStr = Configuration.GetConnectionString("DockerProjectContext");
+                }
+                else
+                {
+                    // Heroku provides PostgreSQL connection URL via env variable
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                }
+
+                options.UseNpgsql(connStr);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
