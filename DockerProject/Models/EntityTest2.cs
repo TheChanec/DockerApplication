179d6557bce70d6544b87a31b0944436bdd7fe28
Enrique Chan <enrique.nec@gmail.com>
2019-05-15 16:20:57 -0500

    Create EntityTest2.cs

---
+++ DockerProject/Models/EntityTest2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerProject
+{
+    public class EntityTest2
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+    }
+}
