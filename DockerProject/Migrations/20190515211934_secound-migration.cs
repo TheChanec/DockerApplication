3b6cc7cf1ff678dbb9d08b64e6abc1af22303de4
Enrique Chan <enrique.nec@gmail.com>
2019-05-15 16:21:26 -0500

    secound migration

---
+++ DockerProject/Migrations/20190515211934_secound-migration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DockerProject.Migrations
+{
+    public partial class secoundmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MyProperty",
+                table: "EntityTest",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "EntityTest2",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityTest2", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntityTest2");
+
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "EntityTest");
+        }
+    }
+}
